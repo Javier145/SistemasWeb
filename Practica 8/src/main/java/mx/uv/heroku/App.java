2f445a8ea95dd939938c4a7709b76993c1a76a99
Javier145 <42722752+Javier145@users.noreply.github.com>
2020-12-11 22:32:10 -0600

    Ejemplo Heroku

---
+++ Practica 8/src/main/java/mx/uv/heroku/App.java
@@ -0,0 +1,20 @@
+package mx.uv.heroku;
+
+import static spark.Spark.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        get("/", (req, res) -> "Hello Heroku World :3");
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+}
